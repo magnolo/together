e9c8e32f37478b12d13f7da026ab271c198c6ca7
magnolo <office@manfredwalder.at>
2016-12-07 19:58:02 +0100

    update login and register

--- angular/app/modules/register/register.module.js
+++ angular/app/modules/register/register.module.js
@@ -14,10 +14,10 @@
             url      : '/register',
             views    : {
                 'main@'                          : {
-                    templateUrl: 'app/core/layouts/content-only.html',
+                    templateUrl: 'views/app/modules/core/layouts/content-only.html',
                     controller : 'MainController as vm'
                 },
-                'content@app.pages_auth_register-v2': {
+                'content@app.register': {
                     templateUrl: 'views/app/modules/register/register.html',
                     controller : 'RegisterV2Controller as vm'
                 }
